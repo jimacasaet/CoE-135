f73cddd3b4f4ab7ecb62db05528fa82458833836
John <macasaetjohn@gmail.com>
2018-09-16 18:05:44 +0800

    working countodwn interrupt

--- Lab 04/lab04.c
+++ Lab 04/lab04.c
@@ -4,18 +4,21 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <stdbool.h>
+#include <string.h>
 #include <sys/time.h>
 
 #define LINE_LEN 30
 #define ARRAY_SIZE 20
+#define COUNTDOWN_TIME 10 //set countdown time in seconds
 
 int done = 0;
+char answers[ARRAY_SIZE][LINE_LEN];
 
 /* this function is run by the second thread */
 void *countdown(void *count_void_ptr){
-    /* increment x to 100 */
+    /* countdown to COUNTDOWN_TIME seconds */
     int *count_ptr = (int *)count_void_ptr;
-    while(++(*count_ptr) < 5){   
+    while(++(*count_ptr) < COUNTDOWN_TIME){   
         sleep(1);
     }
     printf("Time's UP!\n");
@@ -28,8 +31,7 @@ void *accept_input(void *answer_void_ptr){
     char *answer_ptr = (char *)answer_void_ptr;
     printf("Enter your answers: ");
     for(; done != 1;i++){
-        //scanf("%s", answer_ptr);
-        fgets(answer_ptr,LINE_LEN,stdin);
+        scanf("%s",answers[i]);
     }
     return NULL;
 }
@@ -39,7 +41,7 @@ int main(){
     int timer, count = 0, y = 0, i=0;
     char answer[ARRAY_SIZE][LINE_LEN];
 
-    /* initialize reference to second astatic cnd third thread */
+    /* initialize reference to second and third thread */
     pthread_t counter_thread, accept_thread;
     printf("Welcome to Family Feud!\nQuestion: According to survey, what are the top 5 activites people do during the summer?\n");
     for(timer=3;timer>0;timer--){
@@ -64,11 +66,21 @@ int main(){
         return 2;
     }
     //pthread_exit(NULL);
+    pthread_cancel(accept_thread);
+
+    for(count = 0, i=0; i < 20; i++){
+        if(strlen(answers[i])!=0){
+            count++;
+            printf("line %d: %s \ncount: %d\n",i, answers[i],count);
+        }
+    }
 
     printf("Count: %d\nShe said: \n", count);
-    for(;i<count;i++){
-        printf("%s\n",answer[i]);
+    
+    for(i=0;i<count;i++){
+        printf("%s\n",answers[i]);
     }
+    
 
     return 0;
 
