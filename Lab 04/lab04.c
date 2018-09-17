59c6f59ecc5b1994733bc1220129bc1f04eca275
John <macasaetjohn@gmail.com>
2018-09-17 15:49:21 +0800

    final?

--- Lab 04/lab04.c
+++ Lab 04/lab04.c
@@ -9,7 +9,7 @@
 
 #define LINE_LEN 30
 #define ARRAY_SIZE 20
-#define COUNTDOWN_TIME 10 //set countdown time in seconds
+#define COUNTDOWN_TIME 12 //set countdown time in seconds
 #define MAX_ANSWER 5 /* Number of answers to be considered */
 
 int done = 0;
@@ -22,7 +22,7 @@ void *countdown(void *count_void_ptr){
     while(++(*count_ptr) < COUNTDOWN_TIME){   
         sleep(1);
     }
-    printf("Time's UP!\n");
+    printf("\nTime's UP!\n");
     done = 1;
     return NULL;
 }
@@ -30,7 +30,7 @@ void *countdown(void *count_void_ptr){
 void *accept_input(void *answer_void_ptr){
     int i=0;
     char *answer_ptr = (char *)answer_void_ptr;
-    printf("Enter your answers: ");
+    printf("GO!\n\n");
     for(; done != 1;i++){
         scanf("%s",answers[i]);
     }
@@ -40,7 +40,7 @@ void *accept_input(void *answer_void_ptr){
 int main(){
 
     int timer, count = 0, y = 0, i=0, j=0, score=0;
-    int sub_score, k=0;
+    int sub_score, k, exist=0;
     char answer[MAX_ANSWER][LINE_LEN]; /* store first 5 answers to this variable */
     char filtered[MAX_ANSWER][LINE_LEN]; /* store filetered unique answers */
     char key[MAX_ANSWER][LINE_LEN] = {"swimming","eating","vacation","hiking","sleeping"};
@@ -60,7 +60,7 @@ int main(){
         return 1;
     }
     /* create a third thread for input */
-    if(pthread_create(&accept_thread, NULL, accept_input, &answer)) {
+    if(pthread_create(&accept_thread, NULL, accept_input, &answer)){
         fprintf(stderr, "Error creating thread\n");
         return 1;
     }
@@ -72,14 +72,6 @@ int main(){
     }
 
     pthread_cancel(accept_thread);
-
-    /* count number of elements in answer
-    for(count = 0, i=0; i < ARRAY_SIZE; i++){
-        if(strlen(answers[i])!=0){
-            count++;
-            printf("line %d: %s \ncount: %d\n",i, answers[i],count);
-        }
-    } */
 
     /* get first five elements of answer */
     for(i = 0; i<5; i++){
@@ -94,37 +86,61 @@ int main(){
         }
     }
 
-    printf("Player's Answers: \n");
-    for(i=0;i<count;i++){
+    printf("\nPlayer's Answers: \n");
+    for(i=0;i<MAX_ANSWER;i++){
+        if(strcmp(answer[i],"")==0){
+            count = i;
+            break;
+        }
         printf("%s\n",answer[i]);
     }
 
+    //printf("\nFiltered answers:\n");
+    /* get unique answers*/
+    for(i=0,k=0;i<count;i++){
+        //printf("Checking %d \"%s\" \n",i,answer[i]);
+        for(j=0,exist=0;j<k;j++){
+            if(strcmp(answer[i],filtered[j])==0){
+                //printf("\"%s\" exists\n",answer[i]);
+                exist++;
+            }
+        }
+        if(exist==0){
+            strcpy(filtered[j],answer[i]);
+            //printf("%d added \"%s\" to list\n",k,filtered[k]);
+            k++;
+        }
+    }
+    count = k;
     /* compute score */
-    for(i=0; i<count; i++){
+    for(i=0,k=0; i<count; i++){
         for(j=0, sub_score=0; j<MAX_ANSWER; j++){
-            if(strcmp(answer[i],key[j])==0){
+            if(strcmp(filtered[i],key[j])==0){
                 score++;
                 sub_score++;
             }
         }
         if(sub_score==0){
-            strcpy(wrong[k],answer[i]);
+            strcpy(wrong[k],filtered[i]);
             k++;
         }
     }
     
-    printf("Correct Answers: \n");
-    for(i=0;i<5;i++){
+    printf("\nCorrect Answers: \n");
+    for(i=0;i<MAX_ANSWER;i++){
         printf("%s\n",key[i]);
     }
 
-    printf("Incorrect Answers: \n");
+    printf("\nIncorrect Answers (%d): \n",k);
     if(k!=0){
         for(i=0;i<k;i++){
+            if(strcmp(wrong[i],"")==0){
+                break;
+            }
             printf("%s\n",wrong[i]);
         }
     }
-    printf("Your score is: %d out of 5\n",score);
+    printf("\nYou got %d/%d numbers correctly\n",score,MAX_ANSWER);
 
     return 0;
 
