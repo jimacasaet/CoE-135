2aaea05df65d9d5f979e14d592773f7a94bc2b3e
John <macasaetjohn@gmail.com>
2018-09-17 14:02:50 +0800

    working

--- Lab 04/lab04.c
+++ Lab 04/lab04.c
@@ -10,9 +10,10 @@
 #define LINE_LEN 30
 #define ARRAY_SIZE 20
 #define COUNTDOWN_TIME 10 //set countdown time in seconds
+#define MAX_ANSWER 5 /* Number of answers to be considered */
 
 int done = 0;
-char answers[ARRAY_SIZE][LINE_LEN];
+char answers[ARRAY_SIZE][LINE_LEN];  /* store answers from scanf */
 
 /* this function is run by the second thread */
 void *countdown(void *count_void_ptr){
@@ -38,8 +39,12 @@ void *accept_input(void *answer_void_ptr){
 
 int main(){
 
-    int timer, count = 0, y = 0, i=0;
-    char answer[ARRAY_SIZE][LINE_LEN];
+    int timer, count = 0, y = 0, i=0, j=0, score=0;
+    int sub_score, k=0;
+    char answer[MAX_ANSWER][LINE_LEN]; /* store first 5 answers to this variable */
+    char filtered[MAX_ANSWER][LINE_LEN]; /* store filetered unique answers */
+    char key[MAX_ANSWER][LINE_LEN] = {"swimming","eating","vacation","hiking","sleeping"};
+    char wrong[MAX_ANSWER][LINE_LEN];
 
     /* initialize reference to second and third thread */
     pthread_t counter_thread, accept_thread;
@@ -65,22 +70,61 @@ int main(){
         fprintf(stderr, "Error joining thread\n");
         return 2;
     }
-    //pthread_exit(NULL);
+
     pthread_cancel(accept_thread);
 
-    for(count = 0, i=0; i < 20; i++){
+    /* count number of elements in answer
+    for(count = 0, i=0; i < ARRAY_SIZE; i++){
         if(strlen(answers[i])!=0){
             count++;
             printf("line %d: %s \ncount: %d\n",i, answers[i],count);
         }
+    } */
+
+    /* get first five elements of answer */
+    for(i = 0; i<5; i++){
+        if(strcmp(answers[i],"")==0){
+            count = i;
+            //printf("count %d\n",count);
+            break;
+        }
+        else{
+            strcpy(answer[i],answers[i]);
+            printf("%d %s\n",i, answer[i]);
+        }
     }
 
-    printf("Count: %d\nShe said: \n", count);
-    
+    printf("Player's Answers: \n");
     for(i=0;i<count;i++){
-        printf("%s\n",answers[i]);
+        printf("%s\n",answer[i]);
+    }
+
+    /* compute score */
+    for(i=0; i<count; i++){
+        for(j=0, sub_score=0; j<MAX_ANSWER; j++){
+            if(strcmp(answer[i],key[j])==0){
+                score++;
+                sub_score++;
+            }
+        }
+        if(sub_score==0){
+            strcpy(wrong[k],answer[i]);
+            k++;
+        }
     }
     
+    printf("Correct Answers: \n");
+    for(i=0;i<5;i++){
+        printf("%s\n",key[i]);
+    }
+
+    printf("Incorrect Answers: \n");
+    if(k!=0){
+        for(i=0;i<k;i++){
+            printf("%s\n",wrong[i]);
+        }
+    }
+    printf("Your score is: %d out of 5\n",score);
 
     return 0;
 
