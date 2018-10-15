15a3b9513a5ca27f7843e26ae679a86dee143ac5
John <macasaetjohn@gmail.com>
2018-10-15 15:50:18 +0800

    updated lab 07

---
+++ Lab_07/lab7_macasaet.c
@@ -0,0 +1,57 @@
+/*
+ * John Rufino I. Macasaet
+ * 2013-18722
+ * CoE 135 HLM FVW
+ */
+
+#include <linux/proc_fs.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/sched.h>
+#include <asm/uaccess.h>
+
+int len, temp;
+
+char *msg;
+
+int read_proc(struct file *filp,char *buf,size_t count,loff_t *offp ) 
+{
+
+if(count>temp)
+{
+count=temp;
+}
+temp=temp-count;
+copy_to_user(buf,msg, count);
+if(count==0)
+temp=len;
+   
+return count;
+}
+
+struct file_operations proc_fops = {
+read: read_proc
+};
+void create_new_proc_entry() 
+{
+proc_create("hello",0,NULL,&proc_fops);
+
+msg=" Hello World ";
+len=strlen(msg);
+temp=len;
+printk(KERN_INFO "1.len=%d",len);
+}
+
+
+int proc_init (void) {
+ create_new_proc_entry();
+ return 0;
+}
+
+void proc_cleanup(void) {
+ remove_proc_entry("hello",NULL);
+}
+
+MODULE_LICENSE("GPL"); 
+module_init(proc_init);
+module_exit(proc_cleanup);
