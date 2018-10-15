581f8c810b3eb58b689edb1ba1aeac6553920b77
root <macasaetjohn@gmail.com>
2018-10-15 20:36:09 +0800

    final version

--- Lab_07/lab7_macasaet.c
+++ Lab_07/lab7_macasaet.c
@@ -4,78 +4,93 @@
  * CoE 135 HLM FVW
  */
 
+#include <linux/init.h>
+#include <linux/kernel.h>  
 #include <linux/module.h>
 #include <linux/moduleparam.h>
-#include <linux/init.h>
-#include <linux/kernel.h>   
 #include <linux/proc_fs.h>
 #include <linux/uaccess.h>
-#define BUFSIZE  100
+#define BUFFER 128
 
 MODULE_LICENSE("GPL"); 
 MODULE_AUTHOR("John Rufino Macasaet");
-MODULE_DESCRIPTION("Prints nth triangular number.");
+MODULE_DESCRIPTION("Prints nth triangular number to /proc/macasaet_procfs");
 
-static int irq=20;
-module_param(irq,int,0660);
+static int n=1; // default value of n is 1
+module_param(n,int,0);
 
-static int mode=1;
-module_param(mode,int,0660);
 
-static struct proc_dir_entry *ent;
+static struct proc_dir_entry *mymod;
 
-static ssize_t mywrite(struct file *file, const char __user *ubuf,size_t count, loff_t *ppos) 
-{
-	int num,c,i,m;
-	char buf[BUFSIZE];
-	if(*ppos > 0 || count > BUFSIZE)
-		return -EFAULT;
-	if(copy_from_user(buf,ubuf,count))
-		return -EFAULT;
-	num = sscanf(buf,"%d %d",&i,&m);
-	if(num != 2)
-		return -EFAULT;
-	irq = i; 
-	mode = m;
-	c = strlen(buf);
-	*ppos = c;
-	return c;
-}
-
-static ssize_t myread(struct file *file, char __user *ubuf,size_t count, loff_t *ppos) 
-{
-	char buf[BUFSIZE];
+/* Define read for file operations */
+static ssize_t read_op(struct file *file, char __user *ubuf,size_t count, loff_t *ppos) {
+	char buf[BUFFER];
 	int len=0;
-	if(*ppos > 0 || count < BUFSIZE)
+
+    /* Calculate the nth triangular number */
+    int n_tri = (n*(n+1))/2;
+
+    /* error checking */
+	if(*ppos > 0 || count < BUFFER){
 		return 0;
-	len += sprintf(buf,"irq = %d\n",irq);
-	len += sprintf(buf + len,"mode = %d\n",mode);
+    }
+    /* Write nth triangular number to  */
+	len += sprintf(buf,"n = %d\nResult = %d\n",n, n_tri);
+    //printk(KERN_INFO "n = %d Result = %d\n", n, (n*(n+1))/2);
 	
-	if(copy_to_user(ubuf,buf,len))
-		return -EFAULT;
+    /* copy to user space */
+	if(copy_to_user(ubuf,buf,len)){
+		return -EFAULT; // check if bad address
+    }
+
 	*ppos = len;
 	return len;
 }
 
-static struct file_operations myops = 
-{
+/* Define write operation: get data from user space then copy to kernel space */
+static ssize_t write_op(struct file *file, const char __user *ubuf, size_t count, loff_t *ppos){
+	int num, i, var;
+	char buf[BUFFER];
+    /* error checking */
+	if(*ppos > 0 || count > BUFFER){
+		return -EFAULT; 
+    }
+
+    /* copy the data from the user address space to the kernel address space */
+	if(copy_from_user(buf, ubuf, count)){
+		return -EFAULT; // check if bad address
+    }
+
+	num = sscanf(buf, "%d", &i);
+
+	n = i; 
+	var = strlen(buf);
+	*ppos = var;
+
+    /* print nth triangular number to kernel ring buffer */
+    printk(KERN_INFO "n = %d Result = %d\n", n, (n*(n+1))/2);
+	return var;
+}
+
+/* define file op to be passed to proc_create() */
+static struct file_operations operations = {
 	.owner = THIS_MODULE,
-	.read = myread,
-	.write = mywrite,
+	.read = read_op,
+	.write = write_op,
 };
 
-static int simple_init(void)
-{
-	ent=proc_create("mydev",0660,NULL,&myops);
-	printk(KERN_ALERT "LKM inserted.\n");
+/* module_init */
+static int lab7_macasaet_init(void){
+	mymod = proc_create("macasaet_procfs", 0, NULL, &operations);
+	printk(KERN_INFO "LKM inserted.\n");
 	return 0;
 }
 
-static void simple_cleanup(void)
-{
-	proc_remove(ent);
-	printk(KERN_WARNING "LKM removed.\n");
+/* module_exit */
+static void lab7_macasaet_cleanup(void){
+	proc_remove(mymod);
+	printk(KERN_INFO "LKM removed.\n");
 }
 
-module_init(simple_init);
-module_exit(simple_cleanup);
+module_init(lab7_macasaet_init);
+module_exit(lab7_macasaet_cleanup);
